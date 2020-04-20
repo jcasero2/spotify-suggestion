7eb54db2cb3e7d6b5aaafa19d1e0687d6576cc7d
Sid Murthy <sidmurthy221@gmail.com>
2020-04-20 14:54:27 -0400

    mj sucks

--- lyrics.py
+++ lyrics.py
@@ -1,49 +1,142 @@
-import json
-import sys
 import spotipy
-import spotipy.util as util
+import json
+from pprint import pprint
 from spotipy.oauth2 import SpotifyClientCredentials
 
 #export SPOTIFY_CLIENT_ID='90b9f9fc2ec04d04aae543c938299733'
 #export SPOTIFY_CLIENT_SECRET='5b1bcc765ebd4ba2a2bbdb8603705330'
 
-#spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-def show_tracks(tracks):
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name']))
+client_credentials_manager = SpotifyClientCredentials('90b9f9fc2ec04d04aae543c938299733',
+                                                        '5b1bcc765ebd4ba2a2bbdb8603705330')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-##############################
+playlist_id = 'spotify:user:spotifycharts:playlist:3J43GvmqW6M93WKM1sSjO0'
 
-if __name__ == '__main__':
-    if len(sys.argv) > 0:
-        username = sys.argv[1]
-        user_id = sys.argv[2]
-        user_secret = sys.argv[3]
-        user_redirect = sys.argv[4]
-    else:
-        print("Whoops, need your playlist!")
-        print("usage: python user_playlists.py [username]")
-        sys.exit()
+#most important one
+track_array=[]
 
-    token = util.prompt_for_user_token(username, 'user-library-read', user_id, user_secret, user_redirect)
+#array of songs
+song_name_array = []
 
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name'])
-    else:
-        print("Can't get token for", username)
-    #need to first get playlist
+#array of artist ids
+song_id_array = []
 
-    #need to then get the playlist song title, artist and users
+#array of song ids
+song_artist_array = []
 
-    #add them to a 2D array or vector or list or whatnot
+###############################################################3
+#gets the song array out of the paylist tracks
+temp_song_name_array=[]
+offset = 0;
+while True:
+    response = sp.playlist_tracks(playlist_id,
+                                  offset=offset,
+                                  fields=('items.track.name,total'))
+    #pprint(response['items'])
+    temp_song_name_array.append(response['items'])
+    offset = offset + len(response['items'])
 
-    #for each song, output each of the lyrics to a csv file
+    if len(response['items']) == 0:
+        break
 
-    #return the song, artist and lyrics in json format
+temp_song_name_array.pop(1)
+#pprint(temp_song_name_array)
+
+#puts it into a manipulative array
+temp_song_name_array2=[]
+for i in temp_song_name_array:
+    for a in range(1,14): #try to not make this static down the road
+        temp_song_name_array2.append(i[a])
+
+#manipulates array and creates a separate list of strictly song names
+temp_song_name_array3 = []
+for i in temp_song_name_array2:
+    temp_song_name_array3.append(i['track']['name'])
+
+for i in temp_song_name_array3:
+    song_name_array.append(i.encode('ascii', 'ignore')) 
+
+pprint(song_name_array)
+del temp_song_name_array, temp_song_name_array2, temp_song_name_array3
+
+###########################################################################
+#try to generalize above
+#fuck no we can't, maybe put into function
+
+temp_song_id_array=[]
+offset = 0;
+while True: #got this part from an article
+    response = sp.playlist_tracks(playlist_id,
+                                  offset=offset,
+                                  fields=('items.track.id,total'))
+    #pprint(response['items'])
+    temp_song_id_array.append(response['items'])
+    offset = offset + len(response['items'])
+
+    if len(response['items']) == 0:
+        break
+
+temp_song_id_array.pop(1)
+#pprint(temp_song_name_array)
+
+#puts it into a manipulative array
+temp_song_id_array2=[]
+for i in temp_song_id_array:
+    for a in range(1,14): #try to not make this static down the road
+        temp_song_id_array2.append(i[a])
+
+#manipulates array and creates a separate list of strictly song names
+temp_song_id_array3 = []
+for i in temp_song_id_array2:
+    temp_song_id_array3.append(i['track']['id'])
+
+for i in temp_song_id_array3:
+    song_id_array.append(i.encode('ascii', 'ignore')) 
+
+pprint(song_id_array)
+del temp_song_id_array, temp_song_id_array2, temp_song_id_array3
+
+#######################################################################
+#time for artists
+
+temp_song_artist_array=[]
+offset = 0;
+while True: #got this part from an article
+    response = sp.playlist_tracks(playlist_id,
+                                  offset=offset,
+                                  fields=('items.track.artists,total'))
+    #pprint(response['items'])
+    temp_song_artist_array.append(response['items'])
+    offset = offset + len(response['items'])
+
+    if len(response['items']) == 0:
+        break
+
+temp_song_artist_array.pop(1)
+#pprint(temp_song_name_array)
+
+#puts it into a manipulative array
+temp_song_artist_array2=[]
+for i in temp_song_artist_array:
+    for a in range(1,14): #try to not make this static down the road
+        temp_song_artist_array2.append(i[a])
+
+#manipulates array and creates a separate list of strictly song names
+temp_song_artist_array3 = []
+for i in temp_song_artist_array2:
+    temp_song_artist_array3.append(i['track']['artists'])
+
+temp_song_artist_array4 = []
+for i in temp_song_artist_array3:
+    temp_song_artist_array4.append(i[0])
+
+temp_song_artist_array5 = []
+for i in temp_song_artist_array4:
+    temp_song_artist_array5.append(i['name'])
+
+for i in temp_song_artist_array5:
+    song_artist_array.append(i.encode('ascii', 'ignore'))
+
+pprint(song_artist_array)
+del temp_song_artist_array, temp_song_artist_array2, temp_song_artist_array3, temp_song_artist_array4, temp_song_artist_array5
